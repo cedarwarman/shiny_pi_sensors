5008fb8d5d9f736436d84f09ce41a5c520fd2dc4
cedarwarman <cedardalewarman@gmail.com>
2021-08-04 19:08:45 -0700

    Add tee

--- sh/get_rpi_temp.sh
+++ sh/get_rpi_temp.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*'
+vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*' | tee /home/pi/shiny-server/apps/pi_sensor/temperature.txt /home/pi/shiny-server/apps/pi_sensor_home/temperature.txt
